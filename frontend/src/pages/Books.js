6ae338ee668b7d714589ac52a4eca47136e608fc
Bardh Dajaku <bd58352@ubt-uni.net>
2025-12-25 00:22:08 +0100

    Setup frontend and basic login page

---
+++ frontend/src/pages/Books.js
@@ -0,0 +1,4 @@
+function Books() {
+  return <h2>Books Page</h2>;
+}
+export default Books;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("EMAIL:", email);
+    console.log("PASSWORD:", password);
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
